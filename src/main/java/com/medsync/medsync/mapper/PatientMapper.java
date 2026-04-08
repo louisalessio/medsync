8f6b38fa28bc3cc6402be22c77caa7691f96b90f
Louis Alessio <louisalessio.13@gmail.com>
2026-04-08 21:09:31 +0200

    Downgrade to Spring Boot 3.4.1 for better plugin compatibility, Resolve pom.xml duplication and classloader errors

---
+++ src/main/java/com/medsync/medsync/mapper/PatientMapper.java
@@ -0,0 +1,19 @@
+package com.medsync.medsync.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.medsync.medsync.dto.PatientDTO;
+import com.medsync.medsync.model.Patient;
+
+@Mapper(componentModel = "spring")
+public interface PatientMapper {
+
+    // from the DTO to a Entity for the DB
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "gender", ignore = true)
+    Patient toEntity(PatientDTO dto);
+
+    // from the Entity of the DB into a DTO for the client
+    PatientDTO toDto(Patient patient);
+}
--- src/main/java/com/medsync/medsync/service/PatientService.java
+++ src/main/java/com/medsync/medsync/service/PatientService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.medsync.medsync.dto.PatientDTO;
+import com.medsync.medsync.mapper.PatientMapper;
 import com.medsync.medsync.model.Patient;
 import com.medsync.medsync.repository.PatientRepository;
 
@@ -12,9 +13,11 @@ import com.medsync.medsync.repository.PatientRepository;
 public class PatientService {
 
     private final PatientRepository patientRepository;
+    private final PatientMapper patientMapper;
 
-    public PatientService(PatientRepository patientRepository) {
+    public PatientService(PatientRepository patientRepository, PatientMapper patientMapper) {
         this.patientRepository = patientRepository;
+        this.patientMapper = patientMapper;
     }
 
     public List<Patient> getAllPatients() {
@@ -22,13 +25,13 @@ public class PatientService {
     }
 
     public Patient saveFromDto(PatientDTO dto) {
-        Patient patient = new Patient();
-        // mapping of data
-        patient.setFirstName(dto.getFirstName());
-        patient.setLastName(dto.getLastName());
-        patient.setBirthDate(dto.getBirthDate());
-        patient.setEmail(dto.getEmail());
-        patient.setSocialSecurityNumber(dto.getSocialSecurityNumber());
+        Patient patient = patientMapper.toEntity(dto);
         return patientRepository.save(patient);
+    }
+
+    public PatientDTO getPatientById(Long id) {
+        Patient patient = patientRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Patient not found"));
+        return patientMapper.toDto(patient);
     }
 }
